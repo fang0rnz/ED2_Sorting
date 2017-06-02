6e01a6652e28b8b2a73035d24a683f3cff3a29ca
lmoraes <lmoraes@10.9.3.111>
2017-06-02 11:30:57 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: lmoraes
+ *
+ * Created on June 2, 2017, 11:29 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
